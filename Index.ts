7f029f4029f0ab53da465a10b3799541731ad742
Cynic <69977714+Cynic1254@users.noreply.github.com>
2025-02-24 13:13:36 +0100

    initial commit

---
+++ Index.ts
@@ -0,0 +1,14 @@
+﻿import express from "express"
+
+const app = express();
+const port = 19100;
+
+app.use(express.json())
+
+app.post("/api/feedback", (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(port, () => {
+    console.log("Startup_Complete")
+})
